f7b9cfca9d74188b925555dac8a6ad7a4f622741
Burban-Vitalik <burbanvitalik2002@gmail.com>
2021-02-05 19:28:01 +0200

    redux install and begin

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from '../Header/Menu/Menu';
+
+const Header = () => {
+  return(
+    <header>
+        <Menu />
+    </header>
+  )
+}
+
+
+
+export default Header;
